c8252d0a5b5de9d35b775b110426b2a437254f7e
bharani <bharaniprasad4@gmail.com>
2021-06-11 16:22:18 -0500

    this is commit2

--- src/test/java/com/tests/DemoTest.java
+++ src/test/java/com/tests/DemoTest.java
@@ -7,6 +7,8 @@ public class DemoTest {
 	@Test
 	public void testMethod() {
 		System.out.println("We are doing POC on Jenkins");
+		System.out.println("We are doing POC on Jenkins");
+		System.out.println("We are doing POC on Jenkins");
 	}
 
 }
